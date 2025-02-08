e85fd9dec029e32cf057a0e453946bf201bfa1c4
Temchuk <too060802@gmail.com>
2025-02-08 02:17:43 +0200

    3

--- hw-3/hw-3.js
+++ hw-3/hw-3.js
@@ -19,7 +19,7 @@
 // #y7crMeFwHcS
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 {
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i < 10; i++) {
         document.write('<div>Lorem ipsum dolor.</div>');
         console.log([i]);
     }
@@ -32,7 +32,7 @@
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 //
 {
-    for (let i = 0; i <= 10; i++) {
+    for (let i = 0; i < 10; i++) {
         document.write(`<div>Lorem ipsum dolor. ${i} </div>`);
     }
     document.write('<div>******************************************************</div>');
@@ -44,7 +44,7 @@
 
 {
     let i =0;
-    while ( i <= 20  ){
+    while ( i < 20  ){
         document.write(`<h1>Lorem ipsum dolor.</h1>`);
         i++;
     }
@@ -56,7 +56,7 @@
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 {
     let i =0;
-    while ( i <= 20  ){
+    while ( i < 20  ){
         document.write(`<h1>Lorem ipsum dolor. ${i} </h1>`);
         i++;
     }

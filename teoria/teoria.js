6cf92e13a4f508bb004b05a51652a35b5a3fffe5
Temchuk <too060802@gmail.com>
2025-02-18 02:32:26 +0200

    teoria

--- teoria/teoria.js
+++ teoria/teoria.js
@@ -120,6 +120,8 @@
 //     console.log(str.replaceAll('e', '!')); // замінює всі які знайшов
 //     let split = str.split(' '); // розділяє по пробілу і створює  всевдомасив
 //     console.log(split); // ось цей
+// str.trim(); // Метод trim() видаляє всі пробіли з обох кінців стрічки, залишаючи
+// лише текст без зайвих пробілів на початку та в кінці.
 
 // console.log(concat.includes(11)); // чи є  елемент 11
 
@@ -133,7 +135,50 @@
 
 
 
+{
 
+    // let users = [
+    //     {name: 'vasya', age: 31, status: false},
+    //     {name: 'petya', age: 30, status: true},
+    //     {name: 'kolya', age: 29, status: true},
+    //     {name: 'olya', age: 28, status: false},
+    //     {name: 'max', age: 30, status: true},
+    //     {name: 'anya', age: 31, status: false},
+    //     {name: 'oleg', age: 28, status: false},
+    //     {name: 'andrey', age: 29, status: true},
+    //     {name: 'masha', age: 30, status: true},
+    //     {name: 'olya', age: 31, status: false},
+    //     {name: 'max', age: 31, status: true}
+    // ];
+    //
+    //
+    // users.forEach(function (value) {
+    //     console.log(value);
+    // });
+    //
+    //
+    //
+
+    // //
+    // // users.forEach(value => console.log(value));
+    // //
+    // // let filteredUsers = users.filter(value => value.age > 30);
+    // // console.log(filteredUsers);
+    // // console.log(users);
+    // //
+    // // let mapedUsers = users.map(function (value, index) {
+    // //     return {...value, id: index + 1};
+    // // });
+    // // console.log(mapedUsers);
+    // //
+    // // users.map((value, index) => {
+    // //     return {id: index + 1, name: value.name, age: value.age, status: value.status}
+    // // })
+    // //
+    // //
+    // // let find = users.find(value => value.name === 'max');
+    // // console.log(find);
+}
 
 
 
